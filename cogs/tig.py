c3b30adf5397f005cf70c801dc455f76811732d2
rabenkonigin <rabenkonigin@gmail.com>
2026-08-18 09:51:22 +0700

    Add Tig command.

---
+++ cogs/tig.py
@@ -0,0 +1,43 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+from .pimp_my_bot import theme
+
+TIG_NICKNAMES = [
+    "tug", "tuggles", "tiggles", "tiggly", "tiggy", "tuggy", "koktator", "light switch",
+    "tigtator", "The DON", "Tuggernuts", "tiggibug", "Mr. Tigglesworth", "tuggernutter",
+    "tuggleupagus", "tiggums", "Tugs'em", "tigglebutt", "TugLovin", "Tiggymiggy",
+    "tuggy wuggy", "Tig the rig", "flick flick", "flick & lick", "rub a tug tug",
+    "rub a rug n tug", "tiglet", "tugglepus", "Sir Tugsalot", "tiggly wiggly",
+    "TUG-o-RAMA", "OpTigmus Prime! Autokoks roll out!", "King Kok", "Mc Tugglebutt",
+    "Tugglesaur", "Tuggy wuggy woo", "Doc Mc Tiggins", "Tigald Mc Tugalds",
+    "Doc Tugaday", "Tiggy Tig", "Tug of Doom", "Tuglin the Dethroned", "Tuggsy",
+    "Tuggly Bear", "Tiggy Bear", "Tiggy Boo", "TugTug", "tugglebuns", "Tiggy Tuggy",
+    "Big Rig Tig", "tiggle Bittys", "Tig ol' Bitty", "Tiggus", "Tiggy wiggy",
+    "Maximus Tiggus", "Maximus Decimus Tiggius", "Rub-a-tug Tig", "tiggly winks",
+    "gig", "tiggity giggity", "Tig tug", "Tiggamus Maximus", "Tugalug", "Tigatron",
+    "Tiggs McGiggs", "The Tigginator", "Sir McTigguns", "Señor McTigum", "Tigathy",
+    "Tuggy Nuggy", "tigapotamus", "Tigasaurus", "Tigmeister", "Tiggly Tuggly",
+    "Tiggles McGiggles", "Tiggermeister", "MeisterTigger", "Tugglius Maximus",
+    "Tignaught", "Tugganaught", "Tuggernaught", "Tuggle Wuggle", "TuggenaNut",
+    "Tiggidy Wiggley", "Sir Tigsalot", "Kokney", "tug a tug a tug a choooo chooo",
+    "Tugglebum", "Tip tugging tipsy tig", "Tiggy McTugface",
+]
+
+
+class Tig(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(name="tig", description="Show the list of Tig's nickname.")
+    async def tig(self, interaction: discord.Interaction):
+        embed = discord.Embed(
+            title=f"{theme.documentIcon} Tig's Nicknames",
+            description="\n".join(f"• {nickname}" for nickname in TIG_NICKNAMES),
+            color=theme.emColor1
+        )
+        await interaction.response.send_message(embed=embed)
+
+
+async def setup(bot):
+    await bot.add_cog(Tig(bot))
